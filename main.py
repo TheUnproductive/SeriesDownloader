e3879b372fed1d7009af2c682ea96dc3bac00102
justu <geil.js18@gmail.com>
2022-04-05 12:01:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ parser.add_argument("-in", action="store", dest="file", type=str, default="video
 parser.add_argument("-n", action="store", dest="name", type=str, required=True)
 parser.add_argument("-s", action="store", dest="season", type=int, required=True)
 parser.add_argument("-e", action="store", dest="episode", type=int, required=True)
+parser.add_argument("-t", action="store", dest="filetype", type=str, default="mkv")
+parser.add_argument("-v", action=argparse.BooleanOptionalAction, dest="boolean", default=False)
 args = parser.parse_args()
 
 file1 = args.file
 name = args.name
 season = args.season
 episode = args.episode
+ending = "." + args.filetype
+if args.boolean:
+    verbose = " --verbose"
+else:
+    verbose = ""
 
 links_in = open(file1, "r")
 print("It is advised to only load one season at a time")
@@ -25,7 +32,7 @@ if not os.path.isdir(name):
 
 for link in links_in:
     try:
-        os.system("youtube-dl -o download/master.mkv " + link)
+        os.system("youtube-dl -o download/master" + ending + verbose + " " + link)
         if season < 10:
             season_str = "0" + str(season)
         else:
@@ -34,10 +41,11 @@ for link in links_in:
             episode_str = "0" + str(episode)
         else:
             episode_str = str(episode)
-        os.rename("download/master.mkv", name + "/Season " + season_str + "/" + name + " s" + season_str + "e" + episode_str + ".mkv")
+        os.rename("download/master" + ending, name + "/Season " + season_str + "/" + name + " s" + season_str + "e" + episode_str + ending)
         episode = episode + 1
     except:
         print("Error fetching m3u8 info")
+        episode = episode + 1
         pass
 
 links_in.close()

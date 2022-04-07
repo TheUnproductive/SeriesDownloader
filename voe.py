1ad6830126b5f38c53b5ce3b936daadb78a1acb9
justu <geil.js18@gmail.com>
2022-04-07 11:30:04 +0200

    Small formatting changes

--- voe.py
+++ voe.py
@@ -61,40 +61,42 @@ def file_download(name, season, episode, ending, verbose, file, loader):
         else:
             season_str = str(season)
         os.mkdir(name)
-        os.mkdir(name + "/Season " + season_str)
+        os.mkdir("%s/Season %s" % (name, season_str))
         print("\x1b[6;30;42m" + "Created folder %s/Season %s \x1b[0m" % (name, season_str))
 
     for link in links_in:
-        try:
-            cmd = "curl -o data.txt " + link
-            os.system(cmd)
-            data_file = open("data.txt", "r")
-            for line in data_file:
-                if "m3u8" in line:
-                    m3u8_info = line.split('"')
+        if "/--/" in link: pass
+        else:
+            try:
+                cmd = "curl -o data.txt " + link
+                os.system(cmd)
+                data_file = open("data.txt", "r")
+                for line in data_file:
+                    if "m3u8" in line:
+                        m3u8_info = line.split('"')
+                        data_file.close()
+                        os.remove("data.txt")
+                        break
+                print(m3u8_info)
+                for item in m3u8_info:
+                    if "m3u8" in item:
+                        os.system("%s -o download/master%s %s %s" % (loader, ending, verbose, item))
+                        if season < 10:
+                            season_str = "0" + str(season)
+                        else:
+                            season_str = str(season)
+                        if episode < 10:
+                            episode_str = "0" + str(episode)
+                        else:
+                            episode_str = str(episode)
+                        os.rename("download/master" + ending, name + "/Season " + season_str + "/" + name + " s" + season_str + "e" + episode_str + ending)
+                        episode = episode + 1
+            except:
+                print("\x1b[0;30;41m" + "Error fetching m3u8 info\x1b[0m")
+                if data_file:
                     data_file.close()
                     os.remove("data.txt")
-                    break
-            print(m3u8_info)
-            for item in m3u8_info:
-                if "m3u8" in item:
-                    os.system("%s -o download/master%s %s %s" % (loader, ending, verbose, item))
-                    if season < 10:
-                        season_str = "0" + str(season)
-                    else:
-                        season_str = str(season)
-                    if episode < 10:
-                        episode_str = "0" + str(episode)
-                    else:
-                        episode_str = str(episode)
-                    os.rename("download/master" + ending, name + "/Season " + season_str + "/" + name + " s" + season_str + "e" + episode_str + ending)
-                    episode = episode + 1
-        except:
-            print("\x1b[0;30;41m" + "Error fetching m3u8 info\x1b[0m")
-            if data_file:
-                data_file.close()
-                os.remove("data.txt")
-            pass
+                pass
 
     links_in.close()
 
